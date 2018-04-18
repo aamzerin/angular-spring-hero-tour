dfeed71012e9ef8de8234cf6c4f128f594c1a509
Abdessamad Amzerin <aamzerin@everis.com>
2018-04-18 08:35:55 +0100

    added swagger for dev profile

--- spring-hero-tour/src/main/java/io/aamzerin/springherotour/SpringHeroTourApplication.java
+++ spring-hero-tour/src/main/java/io/aamzerin/springherotour/SpringHeroTourApplication.java
@@ -3,7 +3,10 @@ package io.aamzerin.springherotour;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringHeroTourApplication {
 
 	public static void main(String[] args) {
